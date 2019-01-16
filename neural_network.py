30cac9e88d404306cdcb3f162fbf43d4c1c7fe6b
2mawi2 <2mawi2@gmail.com>
2019-01-16 21:15:44 +0100

    Implemented a separate target network

--- neural_network.py
+++ neural_network.py
@@ -11,18 +11,25 @@ import numpy as np
 class NeuralNetwork:
     def __init__(self):
         self.weight_backup = "weights.h5"
-        self.test_mode = True
+        self.test_mode = False
         self.learning_rate = 0.001
         self.memory = deque(maxlen=512)  # buffer for memory replay
         self.sample_batch_size = 32
         self.model = self._build_model()
-        self.batch_counter = 0
+        self.target_model = self._build_model() # we use a separate target network
+        self.target_model_counter = 0
+
+    def _update_target_model(self):
+        self.target_model_counter += 1
+        if self.target_model_counter > self.sample_batch_size: # we update the target model after a batch
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_model_counter = 0
 
     def _build_model(self):
         model = Sequential([  # 6 - 12 - 1
             Dense(12, activation="relu", input_dim=6),
             Dense(12, activation="relu"),
-            #Dense(12, activation='relu'),
+            # Dense(12, activation='relu'),
             #Dense(24, activation='relu'),
             #Dense(24, activation='relu'),
             #Dense(12, activation='relu'),
@@ -43,17 +50,15 @@ class NeuralNetwork:
     def learn(self, inp: NeuralNetworkInput, target: float):
         self.memory.append((inp, target))
 
-        #self.batch_counter += 1
         if len(self.memory) < self.sample_batch_size or self.test_mode:# or self.batch_counter < self.sample_batch_size:
             return
-        #self.batch_counter = 0
 
         sample_batch = random.sample(self.memory, self.sample_batch_size)
-
         x_train = np.array([s.get_state() for s, _ in sample_batch])
         y_train = np.array([t for _, t in sample_batch])
         self.model.fit(x_train, y_train, epochs=10, batch_size=len(sample_batch))
+        self._update_target_model()
 
 
     def get_value(self, inp: NeuralNetworkInput):
-        return self.model.predict(np.array([inp.get_state()]))[0][0]
+        return self.target_model.predict(np.array([inp.get_state()]))[0][0]
