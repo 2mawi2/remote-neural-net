cb934b99e2407b7c1671a7572d70884cd6d59cc4
marius <2mawi2@gmail.xcom>
2019-02-13 19:16:59 +0100

    changed memory to dict implementation

--- neural_network.py
+++ neural_network.py
@@ -23,21 +23,23 @@ class NeuralNetwork:
         self.weight_backup = "weights.h5"
         self._is_learning_mode = True
         self.learning_rate = 0.001
-        self.memory = np.ndarray(shape=(0, 2))
-        self.sample_batch_size = 64
+        self.memory = {}
+        self.sample_batch_size = 32
         self.max_replay_memory_size = 4_194_304
         self.model = self._build_model()
         self.target_model = self._build_model()  # we use a separate target network
         self.graph = tf.get_default_graph()
+        self.epochs = 0
+        self.min_memory_size = 1000
 
         self.target_model_counter = 0
-        self.target_model_update_frequency = 1
+        self.target_model_update_frequency = 200
         # self.tensorboard = TensorBoard(log_dir='./Graph', histogram_freq=0,
         #                               write_graph=True, write_images=True)
 
     def _update_target_model(self):
         self.target_model_counter += 1
-        if self.target_model_counter > self.target_model_update_frequency:
+        if self.target_model_counter >= self.target_model_update_frequency:
             with self.graph.as_default():
                 self.target_model.set_weights(self.model.get_weights())
                 self.target_model._make_predict_function()
@@ -46,8 +48,6 @@ class NeuralNetwork:
     def _build_model(self):
         model = Sequential([
             Dense(26, activation="relu", input_dim=13),
-            Dense(26, activation="relu"),
-            Dense(26, activation="relu"),
             Dense(1, activation='linear'),
         ])
         model._make_predict_function()  # prebuild for concurrency
@@ -72,29 +72,33 @@ class NeuralNetwork:
                 self.target_model.save(self.weight_backup)
 
     def learn(self, experiences):
+        self.epochs += 1
+
+        if self.epochs % 5000 == 0:
+            self.target_model.save(f"weights-{self.epochs}.h5")
+
         for e in experiences:
             inp = NeuralNetworkInput.from_proto(e.state)
             target = e.target
+            self.memory[len(self.memory)] = inp, target
 
-            # contains check really expensive, TODO find faster workaround
-            # if not np.all(np.any(np.isin(self.memory, [inp, target]), axis=0)):
-
-            if len(self.memory) > self.max_replay_memory_size:
-                self.memory = np.delete(self.memory, 0, axis=0)
-            self.memory = np.append(self.memory, [[inp, target]], axis=0)
-
-        if len(self.memory) < self.sample_batch_size or not self._is_learning_mode:
+        if len(self.memory) < self.sample_batch_size \
+                or not self._is_learning_mode \
+                or len(self.memory) < self.min_memory_size:
             return
 
         for _ in range(5):
-            idx = np.random.randint(len(self.memory), size=self.sample_batch_size)
-            mini_batch = self.memory[idx, :]
-            x_train = np.array([s[0].get_state() for s in mini_batch])
+            mini_batch = []
+            for _ in range(self.sample_batch_size):
+                idx = np.random.randint(len(self.memory))
+                state, target = self.memory[idx]
+                mini_batch.append((state.get_state(), target))
+
+            x_train = np.array([s[0] for s in mini_batch])
             y_train = np.array([s[1] for s in mini_batch])
 
             with self.graph.as_default():
-                self.model.fit(x_train, y_train, epochs=1,
-                               batch_size=len(mini_batch))
+                self.model.fit(x_train, y_train, epochs=1, batch_size=len(mini_batch))
                 self._update_target_model()
 
     def get_values(self, inputs: [NeuralNetworkInput]):
